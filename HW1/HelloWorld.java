3573aab8b419876107777c536fa459fa41795f5d
Datto <dattotienn@gmail.com>
2024-02-25 12:49:10 +0700

    aaa
    Datto

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Tô Tiến Đạt\t23020353\tK68AI1\tdattotien\t23020353@vnu.edu.vn");
+        System.out.println("9 bottles of beer on the wall, 9 bottles of beer.\n" +
+                "Take one down, pass it around, \n" +
+                "8 bottles of beer on the wall, 8 bottles of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "...\n" +
+                "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                "Take one down, pass it around,\n" +
+                "No more bottles of beer on the wall.\n");
+    }
+
+}
